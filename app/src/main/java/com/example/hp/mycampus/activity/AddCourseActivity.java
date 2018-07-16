87672453481faf8bb01f46cd6d1ea4df4bf96863
ljsdyspg <issljsdyspg@163.com>
2018-07-16 18:08:59 +0800

    添加界面修改去标题

--- app/src/main/java/com/example/hp/mycampus/activity/AddCourseActivity.java
+++ app/src/main/java/com/example/hp/mycampus/activity/AddCourseActivity.java
@@ -2,7 +2,10 @@ package com.example.hp.mycampus.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +15,12 @@ import android.widget.Toast;
 import com.example.hp.mycampus.R;
 import com.example.hp.mycampus.model.Lesson;
 
-public class AddCourseActivity extends AppCompatActivity {
+public class AddCourseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.activity_add_course);
 
 
