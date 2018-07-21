6e3dfeb0d520865ef8d581d4022094456e42ea72
ljsdyspg <issljsdyspg@163.com>
2018-07-21 20:20:02 +0800

    Revert "返回提交"
    This reverts commit eecdba26b45405df144c65940197eea90065daee.

--- app/src/main/java/com/example/hp/mycampus/activity/LibraryActivity.java
+++ app/src/main/java/com/example/hp/mycampus/activity/LibraryActivity.java
@@ -1,7 +1,6 @@
 package com.example.hp.mycampus.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,7 +8,6 @@ import android.content.Intent;
 import android.content.pm.SharedLibraryInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.View;
 import android.widget.Toast;
 
 import com.example.hp.mycampus.R;
@@ -27,18 +25,6 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("历史借阅");
-        toolbar.setNavigationIcon(R.drawable.btn_back);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(LibraryActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
 
         bookList = LibraryInfo.getHistoryList();
 
